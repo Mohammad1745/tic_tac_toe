e0d303b013179af40c91a9c5e299b98957489c2b
ali <mdali2016.227@gmail.com>
2022-07-01 23:45:52 +0600

    [Add] dynamic method

--- app/Http/Services/Service.php
+++ app/Http/Services/Service.php
@@ -4,12 +4,14 @@
 namespace App\Http\Services;
 
 
+use App\Http\Repositories\Repository;
+
 class Service
 {
     /**
-     * @var
+     * @var Repository
      */
-    private $repository;
+    private Repository $repository;
 
     /**
      * BaseService constructor.
@@ -21,145 +23,12 @@ class Service
     }
 
     /**
+     * @param $method
+     * @param $args
      * @return mixed
      */
-    public function all ()
+    public function __call ($method, $args)
     {
-        return $this->repository->all();
-    }
-
-    /**
-     * @param array $data
-     * @return mixed
-     */
-    public function create (array $data)
-    {
-        return $this->repository->create( $data);
-    }
-
-    /**
-     * @param int $id
-     * @param array $data
-     * @return mixed
-     */
-    public function update (int $id, array $data)
-    {
-        return $this->repository->update( $id, $data);
-    }
-
-    /**
-     * @param array $where
-     * @param array $data
-     * @return mixed
-     */
-    public function updateWhere (array $where, array $data)
-    {
-        return $this->repository->updateWhere( $where, $data);
-    }
-
-    /**
-     * @param array $where
-     * @param array $data
-     * @return mixed
-     */
-    public function updateOrCreate (array $where, array $data){
-        return $this->repository->updateOrCreate( $where, $data);
-    }
-
-    /**
-     * @param int $id
-     * @return mixed
-     */
-    public function delete (int $id) {
-        return $this->repository->delete( $id);
-    }
-
-    /**
-     * @param array $where
-     * @return mixed
-     */
-    public function deleteWhere (array $where)
-    {
-        return $this->repository->deleteWhere( $where);
-    }
-
-    /**
-     * @param array $where
-     * @param array $fields
-     * @return mixed
-     */
-    public function firstWhere (array $where, array $fields=[])
-    {
-        return $this->repository->firstWhere( $where, $fields);
-    }
-
-    /**
-     * @param array $where
-     * @param array $fields
-     * @return mixed
-     */
-    public function lastWhere (array $where, array $fields=[])
-    {
-        return $this->repository->lastWhere( $where, $fields);
-    }
-
-    /**
-     * @param array $where
-     * @param string $field
-     * @return mixed
-     */
-    public function pluckWhere (array $where, string $field)
-    {
-        return $this->repository->pluckWhere( $where, $field);
-    }
-
-    /**
-     * @param array $where
-     * @param array $fields
-     * @return mixed
-     */
-    public function getWhere (array $where, array $fields=[])
-    {
-        return $this->repository->getWhere( $where, $fields);
-    }
-
-    /**
-     * @param array $whereNotIn
-     * @param array $where
-     * @param array $fields
-     * @return mixed
-     */
-    public function getWhereNotIn (array $whereNotIn, array $where=[], array $fields=[])
-    {
-        return $this->repository->getWhereNotIn( $whereNotIn, $where, $fields);
-    }
-
-    /**
-     * @param array $where
-     * @param int $pagination
-     * @param array $fields
-     * @return mixed
-     */
-    public function paginateWhere (array $where, int $pagination=PAGINATE_SMALL, array $fields=[])
-    {
-        return $this->repository->paginateWhere( $where, $pagination,$fields);
-    }
-    /**
-     * @param array $where
-     * @return mixed
-     */
-    public function countWhere (array $where)
-    {
-        return $this->repository->countWhere( $where);
-    }
-
-    /**
-     * @param array $where
-     * @param string $field
-     * @return mixed
-     */
-    public function sumWhere (array $where, string $field)
-    {
-        return $this->repository->sumWhere( $where, $field);
+        return $this->repository->{$method}(...$args);
     }
 }
