48c83b0478eaa1f9b4c214526976cb65062f1f5f
ali <mdali2016.227@gmail.com>
2022-07-01 23:38:26 +0600

    [Add] user model migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string("avatar")->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("username", 80)->unique()->nullable();
+            $table->string("provider_id")->nullable();
+            $table->string("provider")->nullable();
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
