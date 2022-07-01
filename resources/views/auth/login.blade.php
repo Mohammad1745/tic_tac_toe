e94b5a42ad83739bce0435edbf206f3a5880c775
ali <mdali2016.227@gmail.com>
2022-07-02 00:19:23 +0600

    [Add] login page styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,13 +12,32 @@
     <style>
         body {
             font-family: 'Nunito', sans-serif;
+            min-height: 100vh;
+        }
+        .wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+        }
+        .social-btn {
+            text-decoration: none;
+            color: #1a202c;
+            border: 1px solid #1a202c;
+            background: transparent;
+            border-radius: 0.25rem;
+            padding: 0.5rem;
+        }
+        .social-btn:hover {
+            color: white;
+            background: #1a202c;
         }
     </style>
 </head>
 <body class="antialiased">
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<div class="wrapper">
     <div>
-        <a href="http://localhost:8080/auth/redirect?provider=google">Google</a>
+        <a class="social-btn" href="http://localhost:8080/auth/redirect?provider=google">Login with Google</a>
     </div>
 </div>
 </body>
